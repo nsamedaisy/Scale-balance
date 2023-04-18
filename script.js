70d5c995854ba9ddc8726edd9b1083eb9bc7d7aa
nsamedaisy <nsamedaisy@gmail.com>
2023-04-18 12:12:12 +0100

    scale commits

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const leftinput = document.querySelector('#left');
 const rightinput = document.querySelector('#right');
 const button = document.querySelector('#click');
+const arrays = document.querySelector('.weights');
 
-function scaleBalance(arr, num) {
-  for(let i = 0; i < num.length; i++) {
-    for(let j = 0; j < num.length; j++)
-    arr = newarr[i]
+button.addEventListener('click', () => {
+  const newArr = JSON.parse(arr.value);
+
+  for(let i = 0; i < newArr.length; i++) {
+    for(let j = 0; j < newArr.length; j++) {
+      if((leftinput.value + newArr[i]) === (rightinput.value + newArr[j])) {
+        document.querySelector('.displaynum')
+        innerHTML = `${newArr[i]} and ${newArr[j]} are needed to balance ${leftinput.value} and ${rightinput} respectiively`
+        return;
+      }
+      else if (i === newArr.length - 1) {
+        document.querySelector('.displaynone')
+      }
+    }
   }
-}
+})
+
