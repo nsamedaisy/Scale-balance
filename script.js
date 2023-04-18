4bff0a37e31892bddb7300e7188d1fc2f9fc3441
nsamedaisy <nsamedaisy@gmail.com>
2023-04-18 02:36:52 +0100

    scale commits

---
+++ script.js
@@ -0,0 +1,10 @@
+const leftinput = document.querySelector('#left');
+const rightinput = document.querySelector('#right');
+const button = document.querySelector('#click');
+
+function scaleBalance(arr, num) {
+  for(let i = 0; i < num.length; i++) {
+    for(let j = 0; j < num.length; j++)
+    arr = newarr[i]
+  }
+}
